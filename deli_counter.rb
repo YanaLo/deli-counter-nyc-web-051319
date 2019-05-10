8b4e41704a09eaa571ad636ddbc16d8fa2704452
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 10:53:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,6 @@ def take_a_number(katz_deli,name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def 
+def now_serving(katz_deli)
+  if katz_deli.length==0 
+  puts "
